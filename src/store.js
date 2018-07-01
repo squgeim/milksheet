0dbc1a7a57c0e0b874ba276f42ddbcd8c7835301
squgeim <shreyadahal@gmail.com>
2018-07-01 13:32:32 +0545

    Initialise redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const services = {};
+
+const store = createStore(reducer, applyMiddleware(thunk.withExtraArgument(services)));
 
 export default store;
